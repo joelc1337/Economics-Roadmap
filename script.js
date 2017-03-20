f0aaa2371f5dd1365aeaf181aee53a2ebb35f80c
joelc1337 <joelcolon1337@gmail.com>
2017-03-20 18:34:34 -0400

    school update

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 
+$(function(){
+
+	class Event {
+
+		constructor(title,time,length){
+			this.title = title;
+			this.time = time;
+			this.length = length;
+			events.push(this);
+		}
+	}
+
+	var events = []
+
+	var
+	Commute = new Event("Commute","8:00-9:00",1),
+	Work_1 = new Event("Work","9:00-12:00",3),
+	Lunch = new Event("Lunch", "12:00-1:00",1),
+	Meeting = new Event("Meeting","1:00-2:00",1),
+	Work_2 = new Event("Work","2:00-5:00",3)
+
+	console.log(events[0].title);
+
+	for(var i = 0;i <= events.length - 1; i++){
+
+		$('#calendar').append('<div id='+ i + ' class="event"></div>');
+		var $div = $('#' + i);
+
+		var $title = $div.append('<h1>' + events[i].title + '</h1>');
+
+		var $time = $div.append('<h3>' + events[i].time + '</h3>');
+
+
+	}
+
+
+});
